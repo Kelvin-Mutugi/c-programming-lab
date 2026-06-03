4950368368e40ebbd633901ad4479344d3f24e8a
kelvin-mutugi <kelvinmutugi925@gmail.com>
2026-06-04 01:47:58 +0300

    fixes and project student attendance

---
+++ c-fundamentals/04-student-attendance/student_attendance.c
@@ -0,0 +1,5 @@
+/*
+Student Attendance
+Develop a program that prompts the user to enter the number of days attended by each of 10 students out of 30 total school days.
+Using an attendance array and a loop, compute and display the class average attendance percentage to two decimal places.
+*/
