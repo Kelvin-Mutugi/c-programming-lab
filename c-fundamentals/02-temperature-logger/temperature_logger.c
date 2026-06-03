d0f44238f748dc0118e585a02b3d1494e3afec28
kelvin-mutugi <kelvinmutugi925@gmail.com>
2026-06-04 00:13:57 +0300

    entered the main data types

---
+++ c-fundamentals/02-temperature-logger/temperature_logger.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+#include <stdbool.h>
+
+/*
+Temperature Logger
+Develop a program that prompts the user to enter temperature readings for 7 days. Using a temperatures array,
+store each reading via a loop. Compute and display the highest, lowest, and average temperature,
+each to two decimal places.
+*/
+
+int main (){
+
+    float temperatures[7];
+    float highest_temperature = 0.00;
+    float lowest_temperature = 0.00;
+    float average_temperature = 0.00;
+
+    
+
+
+
+
+    return(0);
+}
