42bb8aab231080ee435c9f6fd8a4b8a5674b5bc0
kelvin-mutugi <kelvinmutugi925@gmail.com>
2026-06-04 00:51:18 +0300

    adding inputs to the temparature array

--- c-fundamentals/02-temperature-logger/temperature_logger.c
+++ c-fundamentals/02-temperature-logger/temperature_logger.c
@@ -1,5 +1,6 @@
 #include <stdio.h>
 #include <stdbool.h>
+#define NO_OF_DAYS 7
 
 /*
 Temperature Logger
@@ -10,12 +11,23 @@ each to two decimal places.
 
 int main (){
 
-    float temperatures[7];
+    float temperatures[NO_OF_DAYS];
+    float user_input = 0.00;
     float highest_temperature = 0.00;
     float lowest_temperature = 0.00;
     float average_temperature = 0.00;
 
-    
+    int current_loop = 0;
+    //char days[NO_OF_DAYS] = {"Mon", "Tue", "Wed", "Thur", "Fri", "Sart", "Sun"};
+
+    while (current_loop < NO_OF_DAYS){
+
+        printf("Day %d temperature in Celsius: ", current_loop + 1);
+        scanf("%f", &user_input);
+        temperatures[current_loop] = user_input;
+        current_loop++;
+
+    }
 
 
 
