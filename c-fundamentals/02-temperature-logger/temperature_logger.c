fe408a5c7d2250831223e7315e8edb79b07ab92a
kelvin-mutugi <kelvinmutugi925@gmail.com>
2026-06-04 01:30:25 +0300

    fully functional and refactored temparature logger

--- c-fundamentals/02-temperature-logger/temperature_logger.c
+++ c-fundamentals/02-temperature-logger/temperature_logger.c
@@ -13,14 +13,10 @@ int main (){
 
     float temperatures[NO_OF_DAYS];
     float user_input = 0.00;
-    float highest_temperature = 0.00;
-    float lowest_temperature = 0.00;
+    float total_temp = 0.00;
     float average_temperature = 0.00;
-    float max_temp = temperatures[0];
-    float min_temp = temperatures[0];
 
     int current_loop = 0;
-    //char days[NO_OF_DAYS] = {"Mon", "Tue", "Wed", "Thur", "Fri", "Sart", "Sun"};
 
     while (current_loop < NO_OF_DAYS){
 
@@ -29,18 +25,26 @@ int main (){
         temperatures[current_loop] = user_input;
         current_loop++;
 
-    };
+    }
+
+    float max_temp = temperatures[0];
+    float min_temp = temperatures[0];
 
     for ( int i = 0; i < NO_OF_DAYS; i++){
         if(temperatures[i] > max_temp ){
             max_temp = temperatures[i];
-        };
+        }
         if (temperatures[i] < min_temp){
            min_temp = temperatures[i];
-        };
-        
-    };
+        }
+        total_temp += temperatures[i];
+    }
 
+    average_temperature = total_temp/NO_OF_DAYS;
+
+    printf("highest temp: %.2f\n", max_temp);
+    printf("Lowest temp: %.2f\n", min_temp);
+    printf("average temp: %0.2f\n", average_temperature);
 
 
     return(0);
